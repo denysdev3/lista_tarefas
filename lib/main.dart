cf4386ad2c226deb200de7899feb3fc8eb2ca4f3
Denys Nyckson <denys.dev3@gmail.com>
2020-03-17 20:54:20 -0300

    Start Commit!

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+// ignore: avoid_web_libraries_in_flutter
+import 'dart:html';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
+import 'dart:async';
+import 'dart:convert';
+
+void main (){
+  runApp(MaterialApp(
+    home: Home(),
+  ));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  List _toDoList = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+  Future<File> _getFile() async {
+    final directory = await getApplicationDocumentsDirectory();
+    return File("${directory.path}/data.json");
+  }
+
+  Future<File> _saveData() async {
+    String data = json.encode(_toDoList);
+    final file = await _getFile();
+    return file.writeAsString(data);
+  }
+
+  Future<String> _realData() async{
+    try{
+      final file = await _getFile();
+      return file.readAsString();
+    }
+    catch (e){
+      return null;
+    }
+  }
+}
+
